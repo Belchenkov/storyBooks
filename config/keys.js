6532255a45947e7f3af6ed76e10e346a86a91185
Belchenkov <u608110@gmail.com>
2017-10-18 06:32:11 +0300

    Get Google Auth Client ID

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://belchenkov:mongomaster88@ds121015.mlab.com:21015/storybooks',
+    googleClientID: '248145385233-3brmjoodks0n7mdkt9tfsp7qm83uh7co.apps.googleusercontent.com',
+    googleClientSecret: 'CzixdBwYChcbE5NMyciWFnos'
+};
