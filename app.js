5b5246c5bb76f5926268d3f6caf123a52f7a1995
Belchenkov <u608110@gmail.com>
2017-10-18 10:00:04 +0300

    Adding Session Authentication

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
 
 // Load User Model
 require('./models/User');
@@ -31,8 +34,27 @@ app.get('/', (req, res) => {
     res.send('It Works');
 });
 
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars 
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+
 // Use Routes
 app.use('/auth', auth);
+
+
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
--- config/passport.js
+++ config/passport.js
@@ -42,4 +42,12 @@ module.exports = function(passport) {
         });
       }
     ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    
+    passport.deserializeUser((id, done) => {
+      User.findById(id).then(user => done(null, user));
+  });
 }
