2f06a7416e1c757e6c933962db164b6c87ebd1a1
Belchenkov <u608110@gmail.com>
2017-10-18 09:32:01 +0300

    Saving Authenticated User in MongoDB

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
+// Load user model
+const User = mongoose.model('users');
+
 module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -10,8 +13,33 @@ module.exports = function(passport) {
         proxy: true
       },
       function(accessToken, refreshToken, profile, done) {
-        console.log(accessToken);
-        console.log(profile);
+        // console.log(accessToken);
+        // console.log(profile);
+
+        const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+        
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.firstName,
+          email: profile.emails[0].value,
+          image: image
+        };
+
+        // Check for existing user
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          // Return User
+          if (user) {
+            done(null, user);
+          } else {
+            // Create user
+            new User(newUser)
+              .save()
+              .then(user => done(null, user));
+          }
+        });
       }
     ));
 }
