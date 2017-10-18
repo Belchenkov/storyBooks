7b03f7d9b65e7a3a882d5b850b482986d1aec617
Belchenkov <u608110@gmail.com>
2017-10-18 06:51:55 +0300

    Create GoogleStrategy

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+module.exports = router;
